62abcc730f0dcf959df28a6729f0ed0bfadc9fe6
Vitor Emanuel <tvitorr@gmail.com>
2022-06-29 08:30:05 -0300

    Atualiza classe de armazenamento

--- src/br/com/acme/model/Armazenamento.java
+++ src/br/com/acme/model/Armazenamento.java
@@ -1,13 +1,53 @@
 package br.com.acme.model;
 
+import java.util.ArrayList;
+
 public class Armazenamento {
 
-	private Localizacao[] localizacao;
+    private ArrayList<Localizacao> localizacao;
 
-	private Cliente[] cliente;
+    private ArrayList<Cliente> cliente;
 
-	private Entrega[] entrega;
+    private ArrayList<Entrega> entrega;
 
-	private Drone[] drone;
+    private ArrayList<Drone> drone;
 
+    public Armazenamento() {
+        localizacao = new ArrayList<Localizacao>();
+        cliente = new ArrayList<Cliente>();
+        entrega = new ArrayList<Entrega>();
+        drone = new ArrayList<Drone>();
+    }
+
+    public ArrayList<Localizacao> getLocalizacao() {
+        return localizacao;
+    }
+
+    public void addLocalizacao(Localizacao localizacao) {
+        this.localizacao.add(localizacao);
+    }
+
+    public ArrayList<Cliente> getCliente() {
+        return cliente;
+    }
+
+    public void addCliente(Cliente cliente) {
+        this.cliente.add(cliente);
+    }
+
+    public ArrayList<Entrega> getEntrega() {
+        return entrega;
+    }
+
+    public void addEntrega(Entrega entrega) {
+        this.entrega.add(entrega);
+    }
+
+    public ArrayList<Drone> getDrone() {
+        return drone;
+    }
+
+    public void addDrone(Drone drone) {
+        this.drone.add(drone);
+    }
 }
